806ccf8b45a27c946ca13358b161d01d291aadfe
20syl <20syl.lam78@gmail.com>
2024-04-01 21:56:14 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+import random
+import io
+
+app = Flask(__name__, template_folder="src", static_folder="src")
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('index.html'), 404
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80)
